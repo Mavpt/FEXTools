592e4fe9c62cca857af4cb79667166dc125d8c48
Bruno <bm.silva.oliveira@outlook.com>
2020-03-06 00:00:28 +0000

    Fixed a mistake in main.cpp position

--- ExampleProject/src/main.cpp
+++ ExampleProject/src/main.cpp
@@ -13,17 +13,21 @@ int main()
     DataProperties u_DataProperties("FEXToolsTest", "xAxis", 380.0, 1000.0, "yAxis", 0.4, 1.5);
 
     // DataSet
-    DataSet DataSetTest(u_DataProperties, DrawProperties(), "../InputTest.txt");
-    DataSetTest.Draw("../DataSetTest.eps");
+    DataSet DataSetTest(u_DataProperties, DrawProperties(), "../IO/Data.txt");
+    DataSetTest.Draw("../IO/DataSetTest.eps");
+
+    // FunctionFitter
+    FunctionFitter FunctionFitterTest(u_DataProperties, DrawProperties(), "../IO/Data.txt", "../IO/Function.txt", "../IO/Function.txt");
+    FunctionFitterTest.Draw("../IO/FunctionFitterTest.eps");
 
     // DataStack
     DataStack DataStackTest(u_DataProperties);
-    DataStackTest.Add(DataSetTest);
-    DataStackTest.Draw("../DataStackTest.eps");
+    DataStackTest.Add(&DataSetTest, { kBlue });
+    DataStackTest.Draw("../IO/DataStackTest.eps");
 
-    // FunctionFitter
-    FunctionFitter FunctionFitterTest(u_DataProperties, DrawProperties(), "../InputTest.txt", "../Function.txt", "../Function.txt");
-    FunctionFitterTest.Draw("../FunctionFitterTest.eps");
+    DataStack FunctionDataStackTest(u_DataProperties);
+    FunctionDataStackTest.Add(&FunctionFitterTest, { kBlue, kFullDotLarge, 0.5, kMagenta, kSolid, 1 });
+    FunctionDataStackTest.Draw("../IO/FunctionDataStackTest.eps", 1);
 
     return 0;
 }
